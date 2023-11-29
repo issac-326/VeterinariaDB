d6572e5e966cf1c54afb3f9d9d8bb9945767aa6c
AlbertoRG153 <alberto.romerog27@gmail.com>
2023-11-28 20:02:35 -0600

    Sucursal

--- ProyectoBD/Sucursales.Designer.cs
+++ ProyectoBD/Sucursales.Designer.cs
@@ -34,19 +34,19 @@
             btnModificar = new Button();
             btnSalir = new Button();
             groupBox1 = new GroupBox();
+            label8 = new Label();
+            txtIdDireccion = new TextBox();
+            txtCiudad = new ComboBox();
+            label10 = new Label();
+            txtFarmacia = new TextBox();
+            txtDireccion = new TextBox();
             txtId = new TextBox();
             label9 = new Label();
-            btnDireccion = new Button();
-            btnFarmacia = new Button();
-            txtFarmacia = new ComboBox();
             label7 = new Label();
-            txtGerente = new ComboBox();
             txtEstado = new ComboBox();
-            txtDireccion = new ComboBox();
             txtCorreo = new TextBox();
             txtNombre = new TextBox();
             txtCodigo = new TextBox();
-            label8 = new Label();
             label6 = new Label();
             label5 = new Label();
             label3 = new Label();
@@ -128,19 +128,19 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(txtIdDireccion);
+            groupBox1.Controls.Add(txtCiudad);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(txtFarmacia);
+            groupBox1.Controls.Add(txtDireccion);
             groupBox1.Controls.Add(txtId);
             groupBox1.Controls.Add(label9);
-            groupBox1.Controls.Add(btnDireccion);
-            groupBox1.Controls.Add(btnFarmacia);
-            groupBox1.Controls.Add(txtFarmacia);
             groupBox1.Controls.Add(label7);
-            groupBox1.Controls.Add(txtGerente);
             groupBox1.Controls.Add(txtEstado);
-            groupBox1.Controls.Add(txtDireccion);
             groupBox1.Controls.Add(txtCorreo);
             groupBox1.Controls.Add(txtNombre);
             groupBox1.Controls.Add(txtCodigo);
-            groupBox1.Controls.Add(label8);
             groupBox1.Controls.Add(label6);
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(label3);
@@ -155,139 +155,118 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Formulario";
             // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(565, 203);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 18);
+            label8.TabIndex = 36;
+            label8.Text = "IdDireccion";
+            // 
+            // txtIdDireccion
+            // 
+            txtIdDireccion.Location = new Point(671, 200);
+            txtIdDireccion.Name = "txtIdDireccion";
+            txtIdDireccion.ReadOnly = true;
+            txtIdDireccion.Size = new Size(29, 26);
+            txtIdDireccion.TabIndex = 35;
+            // 
+            // txtCiudad
+            // 
+            txtCiudad.FormattingEnabled = true;
+            txtCiudad.Location = new Point(520, 74);
+            txtCiudad.Margin = new Padding(3, 2, 3, 2);
+            txtCiudad.Name = "txtCiudad";
+            txtCiudad.Size = new Size(240, 26);
+            txtCiudad.TabIndex = 34;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(391, 79);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 18);
+            label10.TabIndex = 32;
+            label10.Text = "Ciudad";
+            // 
+            // txtFarmacia
+            // 
+            txtFarmacia.Location = new Point(122, 186);
+            txtFarmacia.Name = "txtFarmacia";
+            txtFarmacia.ReadOnly = true;
+            txtFarmacia.Size = new Size(240, 26);
+            txtFarmacia.TabIndex = 30;
+            txtFarmacia.TextChanged += txtFarmacia_TextChanged;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(520, 127);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(240, 26);
+            txtDireccion.TabIndex = 29;
+            // 
             // txtId
             // 
-            txtId.Location = new Point(599, 186);
+            txtId.Location = new Point(493, 200);
             txtId.Name = "txtId";
             txtId.ReadOnly = true;
-            txtId.Size = new Size(120, 26);
+            txtId.Size = new Size(29, 26);
             txtId.TabIndex = 28;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(461, 189);
+            label9.Location = new Point(393, 203);
             label9.Name = "label9";
-            label9.Size = new Size(132, 18);
+            label9.Size = new Size(94, 18);
             label9.TabIndex = 27;
-            label9.Text = "IdSeleccionado";
-            // 
-            // btnDireccion
-            // 
-            btnDireccion.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            btnDireccion.BackColor = Color.FromArgb(0, 192, 0);
-            btnDireccion.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDireccion.ForeColor = SystemColors.ActiveCaptionText;
-            btnDireccion.Image = (Image)resources.GetObject("btnDireccion.Image");
-            btnDireccion.ImageAlign = ContentAlignment.MiddleLeft;
-            btnDireccion.Location = new Point(369, 175);
-            btnDireccion.Margin = new Padding(3, 2, 3, 2);
-            btnDireccion.Name = "btnDireccion";
-            btnDireccion.Padding = new Padding(9, 8, 22, 8);
-            btnDireccion.Size = new Size(55, 47);
-            btnDireccion.TabIndex = 26;
-            btnDireccion.TextAlign = ContentAlignment.MiddleRight;
-            btnDireccion.UseVisualStyleBackColor = false;
-            btnDireccion.Click += btnDireccion_Click;
-            // 
-            // btnFarmacia
-            // 
-            btnFarmacia.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            btnFarmacia.BackColor = Color.FromArgb(0, 192, 0);
-            btnFarmacia.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnFarmacia.ForeColor = SystemColors.ActiveCaptionText;
-            btnFarmacia.Image = (Image)resources.GetObject("btnFarmacia.Image");
-            btnFarmacia.ImageAlign = ContentAlignment.MiddleLeft;
-            btnFarmacia.Location = new Point(727, 16);
-            btnFarmacia.Margin = new Padding(3, 2, 3, 2);
-            btnFarmacia.Name = "btnFarmacia";
-            btnFarmacia.Padding = new Padding(9, 8, 22, 8);
-            btnFarmacia.Size = new Size(55, 47);
-            btnFarmacia.TabIndex = 25;
-            btnFarmacia.TextAlign = ContentAlignment.MiddleRight;
-            btnFarmacia.UseVisualStyleBackColor = false;
-            btnFarmacia.Click += btnFarmacia_Click;
-            // 
-            // txtFarmacia
-            // 
-            txtFarmacia.FormattingEnabled = true;
-            txtFarmacia.Location = new Point(461, 27);
-            txtFarmacia.Margin = new Padding(3, 2, 3, 2);
-            txtFarmacia.Name = "txtFarmacia";
-            txtFarmacia.Size = new Size(258, 26);
-            txtFarmacia.TabIndex = 17;
+            label9.Text = "IdSucursal";
+            label9.Click += label9_Click;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(381, 27);
+            label7.Location = new Point(33, 189);
             label7.Name = "label7";
             label7.Size = new Size(83, 18);
             label7.TabIndex = 16;
             label7.Text = "Farmacia";
             // 
-            // txtGerente
-            // 
-            txtGerente.FormattingEnabled = true;
-            txtGerente.Location = new Point(461, 127);
-            txtGerente.Margin = new Padding(3, 2, 3, 2);
-            txtGerente.Name = "txtGerente";
-            txtGerente.Size = new Size(258, 26);
-            txtGerente.TabIndex = 15;
-            // 
             // txtEstado
             // 
             txtEstado.FormattingEnabled = true;
-            txtEstado.Location = new Point(461, 76);
+            txtEstado.Location = new Point(520, 22);
             txtEstado.Margin = new Padding(3, 2, 3, 2);
             txtEstado.Name = "txtEstado";
-            txtEstado.Size = new Size(258, 26);
+            txtEstado.Size = new Size(240, 26);
             txtEstado.TabIndex = 13;
-            // 
-            // txtDireccion
-            // 
-            txtDireccion.FormattingEnabled = true;
-            txtDireccion.Location = new Point(124, 186);
-            txtDireccion.Margin = new Padding(3, 2, 3, 2);
-            txtDireccion.Name = "txtDireccion";
-            txtDireccion.Size = new Size(238, 26);
-            txtDireccion.TabIndex = 12;
-            txtDireccion.SelectedIndexChanged += txtDireccion_SelectedIndexChanged;
             // 
             // txtCorreo
             // 
             txtCorreo.Location = new Point(124, 130);
             txtCorreo.Name = "txtCorreo";
-            txtCorreo.Size = new Size(238, 26);
+            txtCorreo.Size = new Size(240, 26);
             txtCorreo.TabIndex = 10;
             // 
             // txtNombre
             // 
             txtNombre.Location = new Point(124, 79);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(238, 26);
+            txtNombre.Size = new Size(240, 26);
             txtNombre.TabIndex = 9;
             // 
             // txtCodigo
             // 
             txtCodigo.Location = new Point(124, 27);
             txtCodigo.Name = "txtCodigo";
-            txtCodigo.Size = new Size(238, 26);
+            txtCodigo.Size = new Size(240, 26);
             txtCodigo.TabIndex = 8;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(381, 127);
-            label8.Name = "label8";
-            label8.Size = new Size(74, 18);
-            label8.TabIndex = 7;
-            label8.Text = "Gerente";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(381, 82);
+            label6.Location = new Point(391, 25);
             label6.Name = "label6";
             label6.Size = new Size(64, 18);
             label6.TabIndex = 5;
@@ -296,11 +275,11 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(33, 189);
+            label5.Location = new Point(391, 130);
             label5.Name = "label5";
-            label5.Size = new Size(85, 18);
+            label5.Size = new Size(96, 18);
             label5.TabIndex = 4;
-            label5.Text = "Direccion";
+            label5.Text = "Referencia";
             // 
             // label3
             // 
@@ -395,21 +374,21 @@
         private Label label2;
         private Label label1;
         private TextBox txtCodigo;
-        private Label label8;
         private Label label6;
         private Label label5;
         private TextBox txtCorreo;
         private TextBox txtNombre;
-        private ComboBox txtDireccion;
-        private ComboBox txtGerente;
         private ComboBox txtEstado;
         private Label label4;
         private Button btnAgregar;
         private Label label7;
-        private ComboBox txtFarmacia;
-        private Button btnFarmacia;
-        private Button btnDireccion;
         private TextBox txtId;
         private Label label9;
+        private TextBox txtDireccion;
+        private TextBox txtFarmacia;
+        private Label label10;
+        private ComboBox txtCiudad;
+        private Label label8;
+        private TextBox txtIdDireccion;
     }
 }

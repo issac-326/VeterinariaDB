4b2c477e51c346d4d6ed73b43f470e8fb2d260dc
AlbertoRG153 <alberto.romerog27@gmail.com>
2023-12-01 16:03:38 -0600

    Avance Factura

--- ProyectoBD/Factura.cs
+++ ProyectoBD/Factura.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProyectoBD.Class;
 using ProyectoBD.SQLConexion;
 
 namespace ProyectoBD
@@ -23,7 +24,11 @@ namespace ProyectoBD
             this.permisos = permisos;
             this.idSucursal = idSucursal;
             cargarInfo();
+            CargarDatos();
+            //lblEmpresa.Text = null;
         }
+
+
 
         private void cargarInfo()
         {
@@ -43,6 +48,16 @@ namespace ProyectoBD
                 }
             }
             objectConexion.cerrarConexion();
+
+        }
+
+        private void CargarDatos()
+        {
+            Crud crud = new Crud();
+
+            string instruccion = "SELECT TOP 1 e.Nombre as Empresa, d.Referencia, c.Nombre AS Ciudad, de.Nombre AS Departamento,\r\ne.Correo_1 AS Correo, e.RTN AS RTNEmpresa, ins.CAI, f.Num_Factura, p.Primer_Nombre, p.Primer_Apellido,\r\nf.Fecha, f.RTN_Cliente AS RTNCliente, f.Impuesto_15, f.Impuesto_18, f.Total FROM Facturas f \r\nINNER JOIN Inscripcion_SAR ins ON f.Id_Inscripcion = ins.Id\r\nINNER JOIN Responsables_Mascotas rm ON f.Id_Cliente = rm.Id\r\nINNER JOIN Personas p ON rm.Id_Persona = p.Id\r\nINNER JOIN Sucursales s ON f.Id_Scursal = s.Id\r\nINNER JOIN Empresas e ON s.Id_Empresa = e.Id\r\nINNER JOIN Direcciones d ON s.Id_Direccion = d.Id\r\nINNER JOIN Ciudades c ON d.Id_Ciudad = c.Id\r\nINNER JOIN Departamentos de ON c.Id_Departamento = d.Id\r\nORDER BY f.Id DESC;";
+
+            crud.mostrarData(dataGridView1, instruccion);
 
         }
 
@@ -98,8 +113,31 @@ namespace ProyectoBD
 
         private void button4_Click(object sender, EventArgs e)
         {
-            FormularioFactura win = new FormularioFactura(permisos,idSucursal);
+            FormularioFactura win = new FormularioFactura(permisos, idSucursal);
             win.Show();
-            this.Hide();        }
+            this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow filaSeleccionada = dataGridView1.Rows[e.RowIndex];
+
+                lblEmpresa.Text = filaSeleccionada.Cells["Empresa"].Value.ToString();
+                lblDireccion.Text = filaSeleccionada.Cells["Referencia"].Value.ToString() + ", " + filaSeleccionada.Cells["Ciudad"].Value.ToString() + ", " + filaSeleccionada.Cells["Departamento"].Value.ToString();
+                lblCorreo.Text = filaSeleccionada.Cells["Correo"].Value.ToString();
+                lblRtn.Text = filaSeleccionada.Cells["RTNEmpresa"].Value.ToString();
+                lblCai.Text = filaSeleccionada.Cells["CAI"].Value.ToString();
+                lblNumero.Text = filaSeleccionada.Cells["Num_Factura"].Value.ToString();
+                lblCliente.Text = filaSeleccionada.Cells["Primer_Nombre"].Value.ToString() + " " + filaSeleccionada.Cells["Primer_Apellido"].Value.ToString();
+                lblFecha.Text = filaSeleccionada.Cells["Fecha"].Value.ToString();
+                lblRtnCliente.Text = filaSeleccionada.Cells["RTNCliente"].Value.ToString();
+                lblIsv15.Text = filaSeleccionada.Cells["Impuesto_15"].Value.ToString();
+                lblIsv18.Text = filaSeleccionada.Cells["Impuesto_18"].Value.ToString();
+                lblTotal.Text = filaSeleccionada.Cells["Total"].Value.ToString();
+
+            }
+        }
     }
 }
